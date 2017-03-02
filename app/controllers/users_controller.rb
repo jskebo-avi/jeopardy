c610b6c273befcbce1415570030c682413f2add0
Jeff Skebo <jskebo@avifoodsystems.com>
2017-03-02 14:49:10 -0500

    added authorization to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter do
+    redirect_to root_path unless current_user && current_user.admin?
+  end
+
   def index
     @users = User.all
   end
